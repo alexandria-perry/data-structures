b8a429e01c37e3a6947d7d94d4a378a14fe2e885
Alexandria Perry <alexandriaperry@alexandria-perrys-macbook.local>
2018-11-07 13:21:56 -0800

    stack done, queue in progress

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,22 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    size--;
+    return storage[size];
   };
 
   someInstance.size = function() {
+    if (size < 0) {return 0}
+    return size;
   };
 
   return someInstance;
